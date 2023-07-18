4cefd7c8acba85e9e706dbcd9d1e71b637259ffc
enginbukum <yasengin2005@gmail.com>
2023-07-18 14:31:52 +0300

    alternative

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
-import Questions from "./Questions";
 import data from "./data";
+import Questions from "./Questions";
 
 const App = () => {
   const [questions, setQuestions] = useState(data);
+  const [activeId, setActiveId] = useState(null);
+
+  const toggleQuestion = (id) => {
+    const newActiveId = id === activeId ? null : id
+    setActiveId(newActiveId);
+  };
 
   return (
     <main>
-      <Questions questions={questions} />
+      <Questions
+        questions={questions}
+        activeId={activeId}
+        toggleQuestion={toggleQuestion}
+      />
     </main>
   );
 };
+
 export default App;
